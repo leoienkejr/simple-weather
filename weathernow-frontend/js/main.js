f1655e22d50ddb41a8542942f55c79794710f7e2
leoienkejr <leonardoienkejr@gmail.com>
2020-08-07 13:33:05 -0300

    Define callApi()

--- weathernow-frontend/js/main.js
+++ weathernow-frontend/js/main.js
@@ -1,14 +1,16 @@
 window.onload = function(){
     const default_locations = ['tokyo', 'new york', 'london', 'sao paulo', 'beijing'];
     const location =  default_locations[Math.floor(Math.random() * default_locations.length)];
-    console.log(generateApiQuery(location))
+    const api_request_url = generateApiRequestURL(location)
+    callApi(api_request_url).then(console.log)
 }
 
-function generateApiQuery(location_str){
-    const api_url = 'https://leoienkejr-weathernow.herokuapp.com/api/bycityname/'
-    return api_url.concat(location_str)
+function generateApiRequestURL(location_str){
+    const api_url = 'https://leoienkejr-weathernow.herokuapp.com/api/bycityname/';
+    return api_url.concat(location_str);
 }
 
-function callApi(api_query){
-    
+async function callApi(request_url){
+    const response = await fetch(request_url);
+    return response.json();
 }
